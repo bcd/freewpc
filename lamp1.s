c76540e8bc9543ae37845ee9f52df5bf47f21d05
bcd <bcd@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2005-06-26 21:26:39 +0000

    Committing changes before move.
    git-svn-id: svn://localhost/public/freewpc/trunk@9 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- lamp1.s
+++ lamp1.s
@@ -2,26 +2,26 @@
 #include <freewpc.h>
 
 
-.globl _lamp_matrix
-.globl _lamp_flash_select
-.globl _lamp_flash_max
-.globl _lamp_flash_count
-.globl _lamp_apply_delay
+;;;;;.globl _lamp_matrix
+;;;;;.globl _lamp_flash_select
+;;;;;.globl _lamp_flash_max
+;;;;;.globl _lamp_flash_count
+;;;;;.globl _lamp_apply_delay
 
 
 .area sysrom
 
 
-proc(lamp_rtt)
-	lda	_irq_shift_count
-	sta	WPC_LAMP_COL_STROBE
-	lda	_irq_count
-	anda	#0x07
-	ldx	#_lamp_matrix
-	lda	a,x
-	sta	WPC_LAMP_ROW_OUTPUT
-endp
-
+;;;;;proc(lamp_rtt)
+;;;;;	lda	_irq_shift_count
+;;;;;	sta	WPC_LAMP_COL_STROBE
+;;;;;	lda	_irq_count
+;;;;;	anda	#0x07
+;;;;;	ldx	#_lamp_matrix
+;;;;;	lda	a,x
+;;;;;	sta	WPC_LAMP_ROW_OUTPUT
+;;;;;endp
+;;;;;
 
 
 proc(lamp_set_delay)
@@ -77,41 +77,41 @@ proc(lamp_apply)
 endp
 
 
-global(lampset_all)
-	.db	LAMP_OP_RANGE, 0x00, 0x40, LAMP_OP_EXIT
-
-global(lampset_first_half)
-	.db	LAMP_OP_SKIP_RANGE, 0x00, 0x20, LAMP_OP_EXIT
-
-global(lampset_second_half)
-	.db	LAMP_OP_SKIP_RANGE, 0x01, 0x20, LAMP_OP_EXIT
-
-
-proc(_lamp_demo)
-	loop /* main */
-
-		lda	#TIME_33MS
-		jsr	lamp_set_delay
-	
-		ldx	#_lamp_toggle
-		ldy	#lampset_all
-		repeat(a,4)
-			jsr	lamp_apply
-			jsr	c_task_sleep(TIME_100MS * 2)
-		endrep
-	
-		clr	lamp_set_delay
-	
-		ldx	#_lamp_toggle
-		ldy	#lampset_first_half
-		jsr	lamp_apply
-		repeat(a,4)
-			ldy	#lampset_all
-			jsr	lamp_apply
-			jsr	c_task_sleep(TIME_100MS * 2)
-		endrep
-
-	endloop /* main */
-endp
-
+;;;;;global(lampset_all)
+;;;;;	.db	LAMP_OP_RANGE, 0x00, 0x40, LAMP_OP_EXIT
+;;;;;
+;;;;;global(lampset_first_half)
+;;;;;	.db	LAMP_OP_SKIP_RANGE, 0x00, 0x20, LAMP_OP_EXIT
+;;;;;
+;;;;;global(lampset_second_half)
+;;;;;	.db	LAMP_OP_SKIP_RANGE, 0x01, 0x20, LAMP_OP_EXIT
+;;;;;
+;;;;;
+;;;;;proc(_lamp_demo)
+;;;;;	loop /* main */
+;;;;;
+;;;;;		lda	#TIME_33MS
+;;;;;		jsr	lamp_set_delay
+;;;;;	
+;;;;;		ldx	#_lamp_toggle
+;;;;;		ldy	#lampset_all
+;;;;;		repeat(a,4)
+;;;;;			jsr	lamp_apply
+;;;;;			jsr	c_task_sleep(TIME_100MS * 2)
+;;;;;		endrep
+;;;;;	
+;;;;;		clr	lamp_set_delay
+;;;;;	
+;;;;;		ldx	#_lamp_toggle
+;;;;;		ldy	#lampset_first_half
+;;;;;		jsr	lamp_apply
+;;;;;		repeat(a,4)
+;;;;;			ldy	#lampset_all
+;;;;;			jsr	lamp_apply
+;;;;;			jsr	c_task_sleep(TIME_100MS * 2)
+;;;;;		endrep
+;;;;;
+;;;;;	endloop /* main */
+;;;;;endp
+;;;;;
 
