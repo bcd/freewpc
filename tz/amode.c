002348e875712b26232a4d6c846cc8b235d25c81
bcd <bcd@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2005-09-14 01:23:56 +0000

    Add FreeWPC message and test of scrolling up.
    git-svn-id: svn://localhost/public/freewpc/trunk@46 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- tz/amode.c
+++ tz/amode.c
@@ -6,11 +6,20 @@ void amode_deff (void) __taskentry__
 {
 	for (;;)
 	{
+		int i;
+		
 		/** Display game over screen **/
 		dmd_alloc_low_clean ();
 		font_render_string_center (&font_5x5, 64, 16, "GAME OVER");
 		dmd_show_low ();
 		task_sleep_sec (5);
+
+		/** Display FreeWPC message **/
+		dmd_alloc_low_clean ();
+		font_render_string_center (&font_5x5, 64, 13, "FREEWPC");
+		dmd_copy_low_to_high ();
+		dmd_invert_page (dmd_low_buffer);
+		deff_swap_low_high (24, TIME_100MS + TIME_50MS);
 
 		/** Display TZ 2005 message **/
 		dmd_alloc_low_high ();
@@ -18,7 +27,13 @@ void amode_deff (void) __taskentry__
 		font_render_string_center (&font_5x5, 64, 10, "TWILIGHT ZONE");
 		dmd_copy_low_to_high ();
 		font_render_string_center (&font_5x5, 64, 20, "2005");
-		deff_swap_low_high (16, TIME_100MS * 2);
+		deff_swap_low_high (17, TIME_100MS * 2);
+
+		for (i = 32; i != 0; --i)
+		{
+			dmd_shift_up (dmd_low_buffer);
+			task_sleep (TIME_33MS);
+		}
 
 		/** Display last set of player scores **/
 		dmd_alloc_low_clean ();
