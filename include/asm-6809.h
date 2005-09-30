cc93817143c2346baa5988b74270f1f5354425d7
bdominy <bdominy@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2005-09-30 00:16:38 +0000

    Added get_stack_pointer () Added memset, memcpy, and memmove as inlines.
    git-svn-id: svn://localhost/public/freewpc/trunk@95 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- include/asm-6809.h
+++ include/asm-6809.h
@@ -61,14 +61,53 @@ extern inline void __swapbyte (uint8_t *v1, uint8_t *v2)
 
 extern inline void set_stack_pointer (const uint16_t s)
 {
-	asm __volatile__ ("\tlds %0" :: "g" (s) : "d");
+	asm __volatile__ ("lds\t%0" :: "g" (s) : "d");
 }
 
+
+extern inline U16 get_stack_pointer (void)
+{
+	U16 result;
+	asm __volatile__ ("lea%0\t,s" :: "a" (result));
+	return result;
+}
 
 extern inline void set_direct_page_pointer (const uint8_t dp)
 {
 	asm __volatile__ ("\ttfr b, dp" :: "d" (dp));
 }
+
+
+extern inline void *memset (void *s, int c, long unsigned int n)
+{
+	register char *s1 = (char *)s;
+	while (n > 0)
+	{
+		*s1++ = c & 0xFF;
+		n--;
+	}
+	return (s);
+}
+
+
+extern inline void *memcpy (void *s1, const void *s2, long unsigned int n)
+{
+	register char *_s1 = (char *)s1;
+	register char *_s2 = (char *)s2;
+	while (n > 0)
+	{
+		*_s1++ = *_s2++;
+		n--;
+	}
+	return (s1);
+}
+
+
+extern inline void *memmove (void *s1, const void *s2, long unsigned int n)
+{
+	return memcpy (s1, s2, n);
+}
+
 
 
 #endif /* _ASM_6809_H */
