65ec4e3f92dc60fac075cbc6f0f59076fc51fdbc
bdominy <bdominy@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2005-09-30 00:14:41 +0000

    Added more comments. Turned off debugger support by default.  Use -DDEBUGGER to enable it again.
    git-svn-id: svn://localhost/public/freewpc/trunk@94 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- kernel/init.c
+++ kernel/init.c
@@ -11,15 +11,22 @@ __fastram__ uint8_t tick_count;
 uint8_t sys_init_complete;
 
 
-
 void do_fatal (uint16_t pc, errcode_t error_code) __noreturn__
 {
+	U8 *stack = (U8 *)get_stack_pointer () + 16;
+	int i;
+
 	dmd_alloc_low_clean ();
 
 	sprintf ("ERRNO %i", error_code);
-	font_render_string_center (&font_5x5, 64, 4, sprintf_buffer);
-	sprintf ("PC %04x", pc);
-	font_render_string_center (&font_5x5, 64, 16, sprintf_buffer);
+	font_render_string_center (&font_5x5, 64, 2, sprintf_buffer);
+
+	for (i=0; i < 8; i++)
+	{
+		sprintf ("%02x", stack[i]);
+		font_render_string (&font_5x5, i*16, 10, sprintf_buffer);
+	}
+
 	dmd_show_low ();
 	task_dump ();
 	for (;;);
@@ -45,7 +52,9 @@ void do_irq (void)
 
 	/* Execute rtts every 1ms */
 	wpc_led_toggle ();
+#ifdef DEBUGGER
 	db_rtt ();
+#endif
 	switch_rtt ();
 	lamp_rtt ();
 	sol_rtt ();
@@ -137,27 +146,35 @@ void do_reset (void) __noreturn__
 
 	set_direct_page_pointer (0);
 
+	/* Initialize the stack pointer.  We can now make
+	 * function calls!  Note that this stack is only used
+	 * for execution that is not task-based.  Once tasks
+	 * can be run, each task will use its own stack pointer
+	 * separate from this one. */
 	set_stack_pointer (STACK_BASE);
 
+	/* Initialize RAM to all zeroes */
 	ramptr = (uint8_t *)USER_RAM_SIZE;
 	do
 	{
 		*ramptr-- = 0;
 	} while (ramptr != 0);
 
-	// *(volatile uint8_t *)WPC_RAM_LOCK = RAM_UNLOCKED;
-	// *(uint8_t *)WPC_RAM_LOCKSIZE = RAM_LOCK_512;
-	// *(volatile uint8_t *)WPC_RAM_LOCK = RAM_LOCK_512;
+	/* Set up protected RAM */
 	wpc_set_ram_protect (RAM_UNLOCKED);
 	wpc_set_ram_protect_size (RAM_LOCK_512);
 	wpc_set_ram_protect (RAM_LOCK_512);
 
+	/* Initialize the ROM page register so that the
+	 * page of ROM adjacent to the system area is mapped. */
 	wpc_set_rom_page (0x3D);
 
 	sys_init_complete = 0;
 	
 	wpc_led_toggle ();
+#ifdef DEBUGGER
 	db_init ();
+#endif
 	sol_init ();
 	triac_init ();
 	dmd_init ();
@@ -184,10 +201,5 @@ void do_reset (void) __noreturn__
 
 	amode_start ();
 	task_exit ();
-}
-
-#pragma section ("junk")
-void different_section (void)
-{
 }
 
