e773f2cc58e6fe1e83161f4c79e8a9a71630ab43
bcd <bcd@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2005-11-11 04:50:16 +0000

    Splash screen now shows build date and gcc version used.
    git-svn-id: svn://localhost/public/freewpc/trunk@167 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- kernel/reset.c
+++ kernel/reset.c
@@ -1,12 +1,27 @@
 
 #include <freewpc.h>
 
+#define C_STRING(x)	C_STR(x)
+#define C_STR(x)		#x
+
+static char gcc_version[] = C_STRING(GCC_VERSION);
+
+static char build_date[] = __DATE__;
+
 
 void system_reset (void)
 {
 	dmd_alloc_low_clean ();
+
 	sprintf ("FREEWPC V%1x.%02x", FREEWPC_VERSION_MAJOR, FREEWPC_VERSION_MINOR);
 	font_render_string (&font_5x5, 0, 0, sprintf_buffer);
+
+	sprintf ("GCC %s", gcc_version);
+	font_render_string (&font_5x5, 0, 6, sprintf_buffer);
+
+	sprintf ("BUILT %s", build_date);
+	font_render_string (&font_5x5, 0, 12, sprintf_buffer);
+
 	dmd_show_low ();
 
 	task_sleep_sec (3);
