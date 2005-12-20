83fb48e8f91c8760b51c7fc4a5df93efe72dac79
bcd <bcd@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2005-12-20 07:05:41 +0000

    Added FreeWPC acceptance screen.  The first time you use this ROM, you will be prompted to accept the fact that there is no warranty, etc.  After that, the value is saved in nvram so you won't have to do it again.
    git-svn-id: svn://localhost/public/freewpc/trunk@345 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- kernel/reset.c
+++ kernel/reset.c
@@ -1,32 +1,91 @@
 
 #include <freewpc.h>
 
+#define ACCEPT_1	0x19
+#define ACCEPT_2	0x75
+#define ACCEPT_3	0xB9
 
-static char gcc_version[] = C_STRING(GCC_VERSION);
+volatile const char gcc_version[] = C_STRING(GCC_VERSION);
 
 static char build_date[] = __DATE__;
+
+__nvram__ U8 freewpc_accepted[3];
+
+
+#pragma long_branch
+void system_accept_freewpc (void)
+{
+	if ((freewpc_accepted[0] == ACCEPT_1) &&
+		 (freewpc_accepted[1] == ACCEPT_2) &&
+		 (freewpc_accepted[2] == ACCEPT_3))
+		return;
+
+	dmd_alloc_low_clean ();
+	font_render_string_center (&font_5x5, 64, 0, "FREEWPC");
+	font_render_string_center (&font_5x5, 64, 6, "WARNING... BALLY WMS");
+	font_render_string_center (&font_5x5, 64, 12, "DOES NOT SUPPORT");
+	font_render_string_center (&font_5x5, 64, 18, "THIS SOFTWARE");
+	font_render_string_center (&font_5x5, 64, 24, "PRESS ENTER");
+	dmd_show_low ();
+	while (!switch_poll (SW_ENTER)) task_sleep (TIME_66MS);
+
+	dmd_alloc_low_clean ();
+	font_render_string_center (&font_5x5, 64, 0, "FREEWPC");
+	font_render_string_center (&font_5x5, 64, 6, "NO WARRANTY EXISTS");
+	font_render_string_center (&font_5x5, 64, 12, "ROM MAY CAUSE DAMAGE");
+	font_render_string_center (&font_5x5, 64, 18, "TO YOUR MACHINE");
+	font_render_string_center (&font_5x5, 64, 24, "PRESS ENTER");
+	dmd_show_low ();
+	while (!switch_poll (SW_ENTER)) task_sleep (TIME_66MS);
+
+	dmd_alloc_low_clean ();
+	font_render_string_center (&font_5x5, 64, 0, "FREEWPC");
+	font_render_string_center (&font_5x5, 64, 6, "IF YOU ARE SURE YOU");
+	font_render_string_center (&font_5x5, 64, 12, "WANT TO CONTINUE");
+	font_render_string_center (&font_5x5, 64, 18, "PRESS ENTER TWICE");
+	dmd_show_low ();
+	while (!switch_poll (SW_ENTER)) task_sleep (TIME_66MS);
+	while (switch_poll (SW_ENTER)) task_sleep (TIME_66MS);
+	while (!switch_poll (SW_ENTER)) task_sleep (TIME_66MS);
+	while (switch_poll (SW_ENTER)) task_sleep (TIME_66MS);
+
+	dmd_alloc_low_clean ();
+	dmd_show_low ();
+	task_sleep_sec (1);
+
+	wpc_nvram_get ();
+	freewpc_accepted[0] = ACCEPT_1;
+	freewpc_accepted[1] = ACCEPT_2;
+	freewpc_accepted[2] = ACCEPT_3;
+	wpc_nvram_put ();
+}
+#pragma short_branch
 
 
 void system_reset (void)
 {
+	system_accept_freewpc ();
 	dmd_alloc_low_clean ();
 
-	font_render_string (&font_5x5, 0, 0, MACHINE_NAME);
+	font_render_string_center (&font_5x5, 64, 0, MACHINE_NAME);
 
-	sprintf ("SY %1x.%02x GCC %s", 
-		FREEWPC_VERSION_MAJOR, FREEWPC_VERSION_MINOR, gcc_version);
+	sprintf ("SY %1x.%02x   %s", 
+		FREEWPC_VERSION_MAJOR, FREEWPC_VERSION_MINOR, build_date);
 	font_render_string (&font_5x5, 0, 7, sprintf_buffer);
 
-	sprintf ("BUILT %s", build_date);
-	font_render_string (&font_5x5, 0, 14, sprintf_buffer);
+	font_render_string_center (&font_5x5, 64, 21, "TESTING...");
 
 #ifdef USER_TAG
-	font_render_string (&font_5x5, 0, 21, C_STRING(USER_TAG));
+	font_render_string_center (&font_5x5, 64, 14, C_STRING(USER_TAG));
 #endif
 
 	dmd_show_low ();
 
-	task_sleep_sec (3);
+	task_sleep_sec (2);
+	while (sys_init_pending_tasks != 0)
+		task_sleep (TIME_66MS);
+
+	sys_init_complete++;
 
 	dmd_alloc_low_clean ();
 	dmd_show_low ();
