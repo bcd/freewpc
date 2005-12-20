f5578244d88954a25c34f7622df8f8e7839f6df6
bcd <bcd@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2005-12-20 07:09:05 +0000

    Coin door buttons are now always serviced in test mode.
    git-svn-id: svn://localhost/public/freewpc/trunk@350 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- kernel/service.c
+++ kernel/service.c
@@ -2,7 +2,7 @@
 #include <freewpc.h>
 
 
-void sw_escape_button_handler (void) __taskentry__
+void sw_escape_button_handler (void)
 {
 	extern void test_escape_button (void);
 
@@ -10,11 +10,9 @@ void sw_escape_button_handler (void) __taskentry__
 		add_credit ();
 	else
 		test_escape_button ();
-
-	task_exit ();
 }
 
-void sw_down_button_handler (void) __taskentry__
+void sw_down_button_handler (void)
 {
 	extern void test_down_button (void);
 
@@ -22,11 +20,9 @@ void sw_down_button_handler (void) __taskentry__
 
 	if (!in_test)
 		volume_down ();
-
-	task_exit ();
 }
 
-void sw_up_button_handler (void) __taskentry__
+void sw_up_button_handler (void)
 {
 	extern void test_up_button (void);
 
@@ -34,36 +30,37 @@ void sw_up_button_handler (void) __taskentry__
 	
 	if (!in_test)
 		volume_up ();
-
-	task_exit ();
 }
 
-void sw_enter_button_handler (void) __taskentry__
+void sw_enter_button_handler (void)
 {
 	extern void test_enter_button (void);
 
 	test_enter_button ();
-	task_exit ();
 }
 
 
 DECLARE_SWITCH_DRIVER (sw_escape_button)
 {
 	.fn = sw_escape_button_handler,
+	.flags = SW_IN_TEST,
 };
 
 DECLARE_SWITCH_DRIVER (sw_down_button)
 {
 	.fn = sw_down_button_handler,
+	.flags = SW_IN_TEST,
 };
 
 DECLARE_SWITCH_DRIVER (sw_up_button)
 {
 	.fn = sw_up_button_handler,
+	.flags = SW_IN_TEST,
 };
 
 DECLARE_SWITCH_DRIVER (sw_enter_button)
 {
 	.fn = sw_enter_button_handler,
+	.flags = SW_IN_TEST,
 };
 
