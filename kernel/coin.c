483e6a22600beb6206beaa4faab49f867c0ab2f6
bcd <bcd@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2005-12-29 21:45:40 +0000

    Support for .50 cent play Support for nonvolatile storage of coin data
    git-svn-id: svn://localhost/public/freewpc/trunk@400 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- kernel/coin.c
+++ kernel/coin.c
@@ -5,9 +5,11 @@
 
 #define MAX_CREDITS 30
 
-uint8_t free_play;
-uint8_t credit_count;
-uint8_t unit_count;
+__nvram__ U8 free_play;
+__nvram__ U8 credit_count;
+__nvram__ U8 unit_count;
+__nvram__ U8 units_per_credit;
+__nvram__ U8 units_per_coin[4];
 
 void credits_render (void)
 {
@@ -17,7 +19,24 @@ void credits_render (void)
 	if (free_play)
 		sprintf ("FREE PLAY");
 	else
-		sprintf ("CREDITS %d", credit_count);
+	{
+		if (unit_count != 0)
+		{
+			if (credit_count == 0)
+			{
+				sprintf ("CREDITS %d/%d", unit_count, units_per_credit);
+			}
+			else
+			{
+				sprintf ("CREDITS %d %d/%d",
+					credit_count, unit_count, units_per_credit);
+			}
+		}
+		else
+		{
+			sprintf ("CREDITS %d", credit_count);
+		}
+	}
 #endif
 }
 
@@ -25,7 +44,7 @@ void credits_render (void)
 void credits_draw (void)
 {
 	dmd_alloc_low_clean ();
-	dmd_alloc_high_clean ();
+	dmd_alloc_high ();
 
 	credits_render ();
 	font_render_string_center (&font_5x5, 64, 10, sprintf_buffer);
@@ -77,7 +96,10 @@ void add_credit (void)
 	if (credit_count < MAX_CREDITS)
 	{
 #ifndef FREE_ONLY
+		wpc_nvram_get ();
 		credit_count++;
+		wpc_nvram_put ();
+
 		lamp_start_update ();
 #endif
 
@@ -106,10 +128,37 @@ void remove_credit (void)
 #ifndef FREE_ONLY
 	if (credit_count > 0)
 	{
+		wpc_nvram_get ();
 		credit_count--;
+		wpc_nvram_put ();
+
 		lamp_start_update ();
 	}
 #endif
+}
+
+
+void add_units (int n)
+{
+	wpc_nvram_get ();
+	unit_count += n;
+	wpc_nvram_put ();
+
+	if (unit_count >= units_per_credit)
+	{
+		while (unit_count >= units_per_credit)
+		{
+			wpc_nvram_get ();
+			unit_count -= units_per_credit;
+			wpc_nvram_put ();
+
+			add_credit ();
+		}
+	}
+	else
+	{
+		deff_restart (DEFF_CREDITS);
+	}
 }
 
 
@@ -126,32 +175,27 @@ void coin_deff (void) __taskentry__
 
 static void do_coin (uint8_t slot)
 {
-	add_credit ();
-	task_exit ();
+	add_units (units_per_coin[slot]);
 }
 
-void sw_left_coin_handler (void) __taskentry__
+void sw_left_coin_handler (void)
 {
 	do_coin (0);
-	task_exit ();
 }
 
-void sw_center_coin_handler (void) __taskentry__
+void sw_center_coin_handler (void)
 {
 	do_coin (1);
-	task_exit ();
 }
 
-void sw_right_coin_handler (void) __taskentry__
+void sw_right_coin_handler (void)
 {
 	do_coin (2);
-	task_exit ();
 }
 
-void sw_fourth_coin_handler (void) __taskentry__
+void sw_fourth_coin_handler (void)
 {
 	do_coin (3);
-	task_exit ();
 }
 
 
@@ -181,6 +225,8 @@ DECLARE_SWITCH_DRIVER (sw_fourth_coin)
 
 void coin_init (void)
 {
+	/* TODO : this should only be done during a factory reset */
+	wpc_nvram_get ();
 	credit_count = 0;
 	unit_count = 0;
 #ifdef FREE_ONLY
@@ -188,6 +234,13 @@ void coin_init (void)
 #else
 	free_play = FALSE;
 #endif
+	units_per_credit = 2;
+	units_per_coin[0] = 1;
+	units_per_coin[1] = 4;
+	units_per_coin[2] = 1;
+	units_per_coin[3] = 1;
+	wpc_nvram_put ();
+
 	lamp_start_update ();
 }
 
