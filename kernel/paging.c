bbc79f03e7c70442783722520dbf595c06edeff6
bcd <bcd@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2006-01-12 22:25:56 +0000

    Removed old asm code
    git-svn-id: svn://localhost/public/freewpc/trunk@510 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- kernel/paging.c
+++ kernel/paging.c
@@ -15,29 +15,3 @@ uint16_t paged_read_word (uint16_t *word_ptr, uint8_t page)
 {
 	return 0;
 }
-
-#if 0
-proc(paged_lda)   ; X/B = far pointer
-   local(prev_page)
-   uses(b)
-
-   cmpb  page_loaded
-   ifne
-      ldb   page_loaded
-      stb   prev_page
-      ldb   saved(b)
-      stb   page_loaded
-      stb   IO
-
-      lda   ,x
-
-      ldb   prev_page
-      stb   page_loaded
-      stb   IO
-   else
-      lda   ,x
-   endif
-endp
-
-
-#endif
