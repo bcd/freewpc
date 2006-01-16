a295b10250cc2d421184671dd786a05a4c066b33
bcd <bcd@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2006-01-16 04:44:56 +0000

    Replay font is larger.
    git-svn-id: svn://localhost/public/freewpc/trunk@533 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- tz/deffs.c
+++ tz/deffs.c
@@ -4,7 +4,7 @@
 void flash_and_exit_deff (U8 flash_count, task_ticks_t flash_delay)
 {
 	dmd_alloc_low_clean ();
-	font_render_string_center (&font_5x5, 64, 16, sprintf_buffer);
+	font_render_string_center (&font_fixed10, 64, 16, sprintf_buffer);
 	dmd_show_low ();
 	dmd_copy_low_to_high ();
 	dmd_invert_page (dmd_low_buffer);
