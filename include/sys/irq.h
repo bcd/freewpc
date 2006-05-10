eb013efc70200100cb578db884eae0562a96da18
bcd <bcd@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2006-05-10 01:43:24 +0000

    Use compiler builtins to access CC reg, instead of plain old asm macros.
    git-svn-id: svn://localhost/public/freewpc/trunk@733 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- include/sys/irq.h
+++ include/sys/irq.h
@@ -22,15 +22,21 @@
 #define _SYS_IRQ_H
 
 /** How to enable/disable the IRQ */
-#define disable_irq()	asm ("orcc\t#" STR(CC_IRQ))
-#define enable_irq()		asm ("andcc\t#" STR(~CC_IRQ))
+//#define disable_irq()	asm ("orcc\t#" STR(CC_IRQ))
+//#define enable_irq()		asm ("andcc\t#" STR(~CC_IRQ))
+#define disable_irq()	cc_reg |= CC_IRQ
+#define enable_irq()	cc_reg &= ~CC_IRQ
 
-/** How to enable/diable the FIRQ */
-#define disable_firq()	asm ("orcc\t#" STR(CC_FIRQ))
-#define enable_firq()	asm ("andcc\t#" STR(~CC_FIRQ))
+/** How to enable/disable the FIRQ */
+//#define disable_firq()	asm ("orcc\t#" STR(CC_FIRQ))
+//#define enable_firq()	asm ("andcc\t#" STR(~CC_FIRQ))
+#define disable_firq()	cc_reg |= CC_FIRQ
+#define enable_firq()	cc_reg &= ~CC_FIRQ
 
 /** How to enable/disable all interrupts */
-#define disable_interrupts()	asm ("orcc\t#" STR(CC_IRQ|CC_FIRQ))
-#define enable_interrupts()	asm ("andcc\t#" STR(~(CC_IRQ|CC_FIRQ)))
+//#define disable_interrupts()	asm ("orcc\t#" STR(CC_IRQ|CC_FIRQ))
+//#define enable_interrupts()	asm ("andcc\t#" STR(~(CC_IRQ|CC_FIRQ)))
+#define disable_interrupts()	do { disable_irq(); disable_firq(); } while (0)
+#define enable_interrupts()	do { enable_irq(); enable_firq(); } while (0)
 
 #endif /* _SYS_IRQ_H */
