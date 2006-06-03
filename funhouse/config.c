3cdbe4a918f2285db6868da240f9e7e1f792341e
bcd <bcd@5087d79f-0ef8-0310-a22a-8fd82e81f20f>
2006-06-03 18:07:15 +0000

    Updated Funhouse config since hooks removed
    git-svn-id: svn://localhost/public/freewpc/trunk@800 5087d79f-0ef8-0310-a22a-8fd82e81f20f

--- funhouse/config.c
+++ funhouse/config.c
@@ -36,61 +36,6 @@ const uint8_t mach_edge_switches[] = {
 	0x00, 0x70, 0x80, 0x00, 0x00, 0x80, 0x00, 0x00, 0x98, 0x00,
 };
 
-void funhouse_init (void)
-{
-	#include <init.callset>
-}
-
-static void null_hook (void) {}
-
-CALLSET_ENTRY (unused, init) {}
-CALLSET_ENTRY (unused, start_game) {}
-CALLSET_ENTRY (unused, end_game) {}
-CALLSET_ENTRY (unused, start_ball) {}
-
-void funhouse_start_game (void)
-{
-	#include <start_game.callset>
-	task_sleep_sec (2);
-}
-
-
-void funhouse_end_game (void)
-{
-	#include <end_game.callset>
-	if (!in_test)
-		music_set (MUS_POWERBALL_MANIA);
-}
-
-void funhouse_start_ball (void)
-{
-	music_set (MUS_MULTIBALL_LIT_PLUNGER);
-	#include <start_ball.callset>
-}
-
-void funhouse_ball_in_play (void)
-{
-	music_set (MUS_MULTIBALL_LIT);
-	/* start ballsaver if enabled */
-}
-
-bool funhouse_end_ball (void)
-{
-	sound_reset ();
-	return TRUE;
-}
-
-
-void funhouse_add_player (void)
-{
-}
-
-
-void funhouse_any_pf_switch (void)
-{
-}
-
-
 void funhouse_bonus (void)
 {
 	deff_start (DEFF_BONUS);
@@ -100,40 +45,4 @@ void funhouse_bonus (void)
 		task_sleep (TIME_33MS);
 	leff_stop (LEFF_BONUS);
 }
-
-
-void funhouse_tilt (void)
-{
-}
-
-
-void funhouse_tilt_warning (void)
-{
-}
-
-
-void funhouse_start_without_credits (void)
-{
-}
-
-void funhouse_coin_added (void)
-{
-}
-
-machine_hooks_t funhouse_hooks = {
-	INHERIT_FROM_DEFAULT_HOOKS,
-	.start_game = funhouse_start_game,
-	.end_game = funhouse_end_game,
-	.start_ball = funhouse_start_ball,
-	.ball_in_play = funhouse_ball_in_play,
-	.end_ball = funhouse_end_ball,
-	.add_player = funhouse_add_player,
-	.init = funhouse_init,
-	.bonus = funhouse_bonus,
-	.tilt = funhouse_tilt,
-	.tilt_warning = funhouse_tilt_warning,
-	.any_pf_switch = funhouse_any_pf_switch,
-	.start_without_credits = funhouse_start_without_credits,
-	.coin_added = funhouse_coin_added,
-};
 
