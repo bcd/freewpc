09ae4beb4c2d9531db1dff9c5695fcbd2457e9cd
Brian Dominy <brian@oddchange.com>
2007-12-01 13:29:13 -0500

    Don't allow test mode when the coin door is closed.

--- common/service.c
+++ common/service.c
@@ -30,9 +30,23 @@
 /* TODO : coin door may be closed, in which case button presses
 ought to generate a warning message */
 
+void coin_door_buttons_deff (void)
+{
+	dmd_alloc_low_clean ();
+	font_render_string_center (&font_var5, 64, 1, "COIN DOOR IS CLOSED");
+	font_render_string_center (&font_var5, 64, 8, "OPEN COIN DOOR TO");
+	font_render_string_center (&font_var5, 64, 14, "USE BUTTONS");
+	dmd_show_low ();
+	task_sleep_sec (3);
+	deff_exit ();
+}
+
+
 CALLSET_ENTRY (service, sw_escape)
 {
-	if (!in_test)
+	if (!switch_poll_logical (SW_COIN_DOOR_CLOSED))
+		deff_start (DEFF_COIN_DOOR_BUTTONS);
+	else if (!in_test)
 	{
 		add_credit ();
 		audit_increment (&system_audits.service_credits);
@@ -41,7 +55,9 @@ CALLSET_ENTRY (service, sw_escape)
 
 CALLSET_ENTRY (service, sw_down)
 {
-	if (!in_test)
+	if (!switch_poll_logical (SW_COIN_DOOR_CLOSED))
+		deff_start (DEFF_COIN_DOOR_BUTTONS);
+	else if (!in_test)
 		button_invoke (SW_DOWN, volume_down, TIME_500MS, TIME_100MS);
 	else	
 		test_down_button ();
@@ -49,10 +65,26 @@ CALLSET_ENTRY (service, sw_down)
 
 CALLSET_ENTRY (service, sw_up)
 {
-	if (!in_test)
+	if (!switch_poll_logical (SW_COIN_DOOR_CLOSED))
+		deff_start (DEFF_COIN_DOOR_BUTTONS);
+	else if (!in_test)
 		button_invoke (SW_UP, volume_up, TIME_500MS, TIME_100MS);
 	else
 		test_up_button ();
+}
+
+
+void coin_door_opened (void)
+{
+	dbprintf ("Coin door is open\n");
+	/* TODO : Print a message that high power coils are disabled */
+	/* TODO : Enable Stern's "coindoor ballsave" feature */
+}
+
+
+void coin_door_closed (void)
+{
+	dbprintf ("Coin door is closed\n");
 }
 
 
@@ -63,12 +95,8 @@ CALLSET_ENTRY (service, sw_coin_door_closed)
 	event_can_follow (sw_coin_door_closed, sw_slam_tilt, TIME_5S);
 
 	if (switch_poll_logical (SW_COIN_DOOR_CLOSED))
-	{
-		dbprintf ("Coin door is closed\n");
-	}
+		coin_door_closed ();
 	else
-	{
-		dbprintf ("Coin door is open\n");
-	}
+		coin_door_opened ();
 }
 
