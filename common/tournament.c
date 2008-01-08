2ef27f2293292cb051871152ff687afe59142d93
Brian Dominy <brian@oddchange.com>
2008-01-07 19:22:16 -0500

    Tweak player tournament mode message
    Signed-off-by: Brian Dominy <brian@oddchange.com>

--- common/tournament.c
+++ common/tournament.c
@@ -36,8 +36,8 @@ void player_tournament_ready_deff (void)
 	U8 timer;
 
 	dmd_alloc_low_clean ();
-	font_render_string_center (&font_fixed6, 64, 5, "TOURNAMENT MODE");
-	font_render_string_center (&font_fixed6, 64, 15, "ENABLED NOW");
+	font_render_string_center (&font_fixed6, 64, 5, "TOURNAMENT");
+	font_render_string_center (&font_fixed6, 64, 16, "MODE ENABLED");
 	font_render_string_center (&font_var5, 64, 28, "PRESS START NOW");
 	dmd_show_low ();
 
