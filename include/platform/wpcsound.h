4884ccf5e1c3730ad08f2bdd6260f27c7cde818d
Brian Dominy <brian@oddchange.com>
2008-05-14 17:14:37 -0400

    Add more YM2151 definitions.

--- include/platform/wpcsound.h
+++ include/platform/wpcsound.h
@@ -45,14 +45,36 @@ hardware supported volume is. */
 #define EPOT_CLOCK 0x1
 #define EPOT_MAX 128
 
+/* FM chip (Yamaha 2151) registers */
+
+#define DEV_MOD1 0
+#define DEV_MOD2 1
+#define DEV_CAR1 2
+#define DEV_CAR2 3
+
 #define FM_ADDR_TEST 0x1
+
+	#define FM_LFO_RESET 0x2
+
 #define FM_ADDR_KEY_ON 0x8
+
+	#define FM_MOD1 0x40
+	#define FM_CAR1 0x20
+	#define FM_MOD2 0x10
+	#define FM_CAR2 0x8
+	#define FM_KEYCH_MASK 0x7
+
 #define FM_ADDR_NOISE_ENABLE 0xF
-#define FM_ADDR_CLOCK_A1 0x10
-#define FM_ADDR_CLOCK_A2 0x11
-#define FM_ADDR_CLOCK_B 0x12
+
+	#define FM_NOISE_ENABLE 0x80
+	#define FM_NOISE_FREQ_MASK 0x1F
+
+#define FM_ADDR_CLOCK_A1 0x11
+#define FM_ADDR_CLOCK_A2 0x12
+#define FM_ADDR_CLOCK_B 0x13
 #define FM_ADDR_CLOCK_CTRL 0x14
 
+	/* Values for writing to the clock control register */
 	#define FM_TIMER_CSM 0x80
 	#define FM_TIMER_FRESETB 0x20
 	#define FM_TIMER_FRESETA 0x10
@@ -61,6 +83,7 @@ hardware supported volume is. */
 	#define FM_TIMER_LOADB 0x02
 	#define FM_TIMER_LOADA 0x01
 
+	/* Values for reading from the clock control register */
 	#define FM_TIMER_ISTB 0x2
 	#define FM_TIMER_ISTA 0x1
 
@@ -68,9 +91,9 @@ hardware supported volume is. */
 
 #define FM_ADDR_PMD_AMD 0x19
 
-	#define PMD_AMD_SELECT 0x80
-	#define SELECT_PMD 0x80
-	#define SELECT_AMD 0x0
+	#define FM_PMD_AMD_SELECT 0x80
+	#define FM_SELECT_PMD 0x80
+	#define FM_SELECT_AMD 0x0
 
 #define FM_ADDR_CT_W 0x1B
 
@@ -78,18 +101,34 @@ hardware supported volume is. */
 	#define FM_CT1 0x40
 	#define FM_WAVE_SELECT 0x3
 
-		#define PM_JAGGED 0x0
-		#define PM_SQUARE 0x1
-		#define PM_SAW 0x2
-		#define PM_NOISE 0x3
+		#define FM_WAVE_SAW 0x0
+		#define FM_WAVE_SQUARE 0x1
+		#define FM_WAVE_TRIANGLE 0x2
+		#define FM_WAVE_NOISE 0x3
 
-		#define AM_RIGHT_TRIANGLE 0x0
-		#define AM_SQUARE 0x1
-		#define AM_TRIANGLE 0x2
-		#define AM_NOISE 0x3
+#define FM_ADDR_CHAN_CTRL 0x20
 
-#define FM_ADDR_CHANNEL_CTRL 0x20
-#define FM_ADDR_KEYCODE(n) (0x28 + (n))
-#define FM_ADDR_KEYFRAC(n) (0x30 + (n))
+	#define FM_CHAN_RIGHT 0x80
+	#define FM_CHAN_LEFT 0x40
+	#define FM_FB_MASK 0x38
+	#define FM_CONNECT_MASK 0x7
+
+#define FM_ADDR_KEYCODE(ch) (0x28 + (ch))
+
+#define FM_ADDR_KEYFRAC(ch) (0x30 + (ch))
+
+#define FM_ADDR_SENS(ch) (0x38 + (ch))
+
+#define FM_ADDR_DETUNE_MULT(dev,ch)  (0x40 + 8 * (dev) + (ch))
+
+#define FM_ADDR_LEVEL(dev,ch)  (0x60 + 8 * (dev) + (ch))
+
+#define FM_ADDR_ATTACK(dev,ch)  (0x80 + 8 * (dev) + (ch))
+
+#define FM_ADDR_DECAY1(dev,ch)  (0xA0 + 8 * (dev) + (ch))
+
+#define FM_ADDR_DECAY2(dev,ch)  (0xC0 + 8 * (dev) + (ch))
+
+#define FM_ADDR_RELEASE(dev,ch)  (0xE0 + 8 * (dev) + (ch))
 
 #endif /* _WPCSOUND_H */
