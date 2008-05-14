5a139fc1d1c8575d15ebf115f04f635f8cca34d1
Brian Dominy <brian@oddchange.com>
2008-05-14 17:23:07 -0400

    Add YM2151 FM chip module.

---
+++ platform/wpcsound/fm.c
@@ -0,0 +1,50 @@
+
+#include <freewpc.h>
+
+
+/**
+ * Waits until the FM chip is ready to receive a new
+ * read/write operation.
+ */
+static void fm_wait (void)
+{
+}
+
+
+/**
+ * Writes to a FM-chip register.
+ */
+void fm_write (U8 addr, U8 val)
+{
+	fm_wait ();
+	writeb (WPCS_FM_ADDR_STATUS, addr);
+	fm_wait ();
+	writeb (WPCS_FM_DATA, val);
+}
+
+
+/**
+ * Reads from a FM-chip register.
+ */
+U8 fm_read (U8 addr)
+{
+	U8 val;
+
+	fm_wait ();
+	writeb (WPCS_FM_ADDR_STATUS, addr);
+	fm_wait ();
+	val = readb (WPCS_FM_DATA);
+	return val;
+}
+
+
+void fm_init (void)
+{
+	/* Program the FM timer register to generate a periodic
+	interrupt on the FIRQ */
+	fm_write (FM_ADDR_CLOCK_A1, 0xFD);
+	fm_write (FM_ADDR_CLOCK_A2, 0x02);
+	fm_write (FM_ADDR_CLOCK_CTRL,
+		FM_TIMER_FRESETA + FM_TIMER_IRQENA + FM_TIMER_LOADA);
+}
+
