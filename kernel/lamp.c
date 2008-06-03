811dad26517f4cbe6c7e2a734fdc220de73b414c
Brian Dominy <brian@oddchange.com>
2008-06-02 23:39:49 -0400

    Fix recent bug introduced to whole matrix bit test functions.

--- kernel/lamp.c
+++ kernel/lamp.c
@@ -223,20 +223,22 @@ bool bit_test (const_bitset matrix, U8 bit)
 /** Return nonzero if all bits in a matrix are set. */
 bool bit_test_all_on (const_bitset matrix)
 {
-	return !~(matrix[0] & matrix[1]
+	U8 product = (matrix[0] & matrix[1]
 		& matrix[2] & matrix[3]
 		& matrix[4] & matrix[5]
 		& matrix[6] & matrix[7]);
+	return (product == 0xFF);
 }
 
 
 /** Return nonzero if all bits in a matrix are clear. */
 bool bit_test_all_off (const_bitset matrix)
 {
-	return !!~(matrix[0] & matrix[1]
-		& matrix[2] & matrix[3]
-		& matrix[4] & matrix[5]
-		& matrix[6] & matrix[7]);
+	U8 sum = (matrix[0] | matrix[1]
+		| matrix[2] | matrix[3]
+		| matrix[4] | matrix[5]
+		| matrix[6] | matrix[7]);
+	return (sum == 0);
 }
 
 
