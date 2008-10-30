32600deea82ced88f66eb67e54d9767c6153274e
Brian Dominy <brian@oddchange.com>
2008-10-30 07:37:51 -0400

    Change device kickout to use synchronous solenoid request call.

--- common/device.c
+++ common/device.c
@@ -377,7 +377,7 @@ wait_and_recount:
 			/* Pulse the solenoid. */
 			/* TODO - in this task context, we can wait for the queue to
 			be serviced. */
-			sol_request_async (dev->props->sol);
+			sol_request (dev->props->sol);
 
 			/* In timed games, a device kick will pause the game timer.
 			 * TODO : this should be a global event that other modules
