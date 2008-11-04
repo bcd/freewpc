7b43b9c24bb8dad5829843bbc1e033f2517b76d9
Brian Dominy <brian@oddchange.com>
2008-11-03 19:41:18 -0500

    Misc. changes for the DMD test ROM:
    - Manual plunger ballsave compiles.
    - Add upper flippers to test.
    - Exclude flasher test as there are none defined.
    
    --- a/common/ballsave.c
    +++ b/common/ballsave.c
    @@ -85,9 +85,9 @@ static bool ballsave_test_active (void)
    
     void ballsave_launch (void)
     {
    -#ifdef CONFIG_TZ
    +#if defined(CONFIG_TZ)
     	autofire_add_ball ();
    -#else
    +#elif defined (DEVNO_TROUGH)
     	device_request_kick (device_entry (DEVNO_TROUGH));
     #endif
     	deff_start (DEFF_BALL_SAVE);

--- test/window.c
+++ test/window.c
@@ -3341,7 +3341,12 @@ struct menu *test_menu_items[] = {
 	&switch_levels_item,
 	&single_switches_item,
 	&solenoid_test_item,
+#ifndef MACHINE_LACKS_ALL_INCLUDES
+	/* Flasher test is excluded for the DMD-tester
+	 * because nothing is defined as a flasher.
+	 * Everything can be accessed in solenoid test. */
 	&flasher_test_item,
+#endif
 	&gi_test_item,
 	&sound_test_item,
 	&lamp_test_item,
