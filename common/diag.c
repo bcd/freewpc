2a0b78f763c20849e7c458dca77421333c82cbce
Brian Dominy <brian@oddchange.com>
2008-11-16 23:44:10 -0500

    Fix effect for reporting diagnostic errors.

--- common/diag.c
+++ common/diag.c
@@ -7,12 +7,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * FreeWPC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with FreeWPC; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -35,6 +35,42 @@ U8 diag_error_count;
 U8 diag_announce_flag;
 
 
+void
+diag_message_start (void)
+{
+	dmd_alloc_low_high ();
+	dmd_clean_page_low ();
+}
+
+
+void
+diag_message_flash (void)
+{
+	U8 n;
+	dmd_clean_page_high ();
+	for (n = 0; n < 5; n++)
+	{
+		task_sleep (TIME_200MS);
+		dmd_show_high ();
+		task_sleep (TIME_100MS);
+		dmd_show_low ();
+		sound_send (SND_TEST_ALERT);
+	}
+	task_sleep_sec (1);
+	barrier ();
+}
+
+
+void
+diag_message_scroll (void)
+{
+	dmd_sched_transition (&trans_scroll_left);
+	dmd_show_low ();
+	task_sleep_sec (3);
+	barrier ();
+}
+
+
 /**
  * Return the number of diagnostic errors.
  */
@@ -54,16 +90,29 @@ void
 diag_post_error (void_function messenger)
 {
 	diag_error_count++;
+	dbprintf ("Error %d detected.\n", diag_error_count);
 	if (diag_announce_flag && messenger)
 	{
 		if (diag_error_count == 1)
 		{
+			diag_message_start ();
+			font_render_string_center (&font_mono5, 64, 6, "TEST REPORT...");
+			dbprintf ("About to flash...\n");
+			diag_message_flash ();
 		}
-		dmd_alloc_low_clean ();
+		diag_message_start ();
 		messenger ();
-		dmd_show_low ();
-		task_sleep_sec (3);
+		diag_message_scroll ();
 	}
+}
+
+
+void
+diag_run_task (void)
+{
+	diag_error_count = 0;
+	callset_invoke (diagnostic_check);
+	task_exit ();
 }
 
 
@@ -73,11 +122,11 @@ diag_post_error (void_function messenger)
 void
 diag_run (void)
 {
-	/* Reset the error count */
-	diag_error_count = 0;
-
-	/* Invoke each of the diagnostics */
-	callset_invoke (diagnostic_check);
+	/* Create in a separate task context, to avoid
+	 * stack overflow problems. */
+	task_recreate_gid (GID_DIAG_RUNNING, diag_run_task);
+	while (task_find_gid (GID_DIAG_RUNNING))
+		task_sleep (TIME_500MS);
 }
 
 
@@ -87,21 +136,10 @@ CALLSET_ENTRY (diag, init_complete)
 	diag_run ();
 	if (diag_error_count > 0)
 	{
-		U8 n;
-		dmd_alloc_low_high ();
-		dmd_clean_page_low ();
-		dmd_clean_page_high ();
+		diag_message_start ();
 		font_render_string_center (&font_mono5, 64, 10, "PRESS ENTER");
 		font_render_string_center (&font_mono5, 64, 21, "FOR TEST REPORT");
-		for (n = 0; n < 5; n++)
-		{
-			task_sleep (TIME_200MS);
-			dmd_show_high ();
-			task_sleep (TIME_200MS);
-			dmd_show_low ();
-			sound_send (SND_TEST_ALERT);
-		}
-		task_sleep_sec (1);
+		diag_message_flash ();
 	}
 }
 
