874a54b09bfbcae956eb96615853dfcc7e5ecbab
Brian Dominy <brian@oddchange.com>
2009-04-01 22:01:39 -0400

    Implemented much more of a working enter initials screen.

--- common/initials.c
+++ common/initials.c
@@ -19,7 +19,6 @@
  */
 
 #include <freewpc.h>
-#include <window.h>
 
 /**
  * \file
@@ -31,7 +30,11 @@
 
 #define NUM_INITIALS_ALLOWED 3
 
-static const char initial_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ *";
+/** The array of characters that can be entered.
+ * Keep the length of this as a power of 2 (32) so that
+ * the circular buffer implementation is simple.
+ */
+static const unsigned char initial_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ */-+&";
 
 
 /* The amount of time left to enter initials */
@@ -44,65 +47,94 @@ U8 initials_index;
 U8 initials_selection;
 
 /* The array of initials */
-U8 initials_data[NUM_INITIALS_ALLOWED];
+U8 initials_data[NUM_INITIALS_ALLOWED+1];
 
 /* The callback function to invoke when initials are entered OK */
 void (*initials_enter_complete) (void);
 
 
-void initials_enter_init (void)
+void enter_initials_deff (void)
 {
+	U8 n;
+
+	while (task_find_gid (GID_ENTER_INITIALS))
+	{
+		dmd_alloc_low_clean ();
+		font_render_string_left (&font_var5, 2, 3, "ENTER INITIALS");
+		font_render_string_left (&font_fixed6, 2, 15, initials_data);
+		sprintf ("%12s", initial_chars + initials_selection);
+		font_render_string_left (&font_mono5, 2, 26, sprintf_buffer);
+
+		for (n = 25; n <= 31; n++)
+			dmd_low_buffer[16UL * n] ^= 0xFE;
+
+		sprintf ("%d", initials_enter_timer);
+		font_render_string_right (&font_mono5, 126, 3, sprintf_buffer);
+		dmd_show_low ();
+		task_sleep (TIME_300MS);
+	}
+	deff_exit ();
+}
+
+void initials_stop (void)
+{
+	task_kill_gid (GID_ENTER_INITIALS);
+	initials_enter_timer = 0;
+}
+
+
+void initials_running (void)
+{
+	task_sleep_sec (1);
 	initials_enter_timer = 30;
+	memset (initials_data, 0, sizeof (initials_data));
 	initials_index = 0;
 	initials_selection = 0;
-}
+	deff_start (DEFF_ENTER_INITIALS);
 
+#if 1
+	initials_enter_complete = null_function;
+#endif
 
-void initials_enter_draw (void)
-{
-	dmd_alloc_low_clean ();
-	font_render_string_left (&font_fixed6, 2, 9, "ENTER INITIALS");
-	sprintf ("%8s", initial_chars + initials_index);
-	font_render_string_left (&font_fixed6, 2, 22, sprintf_buffer);
-	dmd_show_low ();
-}
-
-
-void initials_enter_left (void)
-{
-	--initials_selection;
-}
-
-
-void initials_enter_right (void)
-{
-	++initials_selection;
-}
-
-
-void initials_enter_start (void)
-{
-	if (++initials_index == NUM_INITIALS_ALLOWED)
+	while (initials_enter_timer > 0)
 	{
-		(*initials_enter_complete) ();
+		task_sleep (TIME_1S + TIME_66MS);
+		initials_enter_timer--;
 	}
+	task_exit ();
 }
-
-
-struct window_ops initials_enter_window = {
-	DEFAULT_WINDOW,
-	.escape = null_function,
-	.init = initials_enter_init,
-	.draw = initials_enter_draw,
-	.left = initials_enter_left,
-	.right = initials_enter_right,
-	.start = initials_enter_start,
-};
 
 
 void initials_enter (void)
 {
-	initials_enter_timer = 30;
-	window_push (&initials_enter_window, NULL);
+	task_create_gid1 (GID_ENTER_INITIALS, initials_running);
+}
+
+
+CALLSET_ENTRY (initials, sw_left_button)
+{
+	if (initials_enter_timer)
+		--initials_selection;
+}
+
+
+CALLSET_ENTRY (initials, sw_right_button)
+{
+	if (initials_enter_timer)
+		++initials_selection;
+}
+
+
+CALLSET_ENTRY (initials, sw_start_button)
+{
+	if (initials_enter_timer)
+	{
+		initials_data[initials_index] = initial_chars[initials_selection];
+		if (++initials_index == NUM_INITIALS_ALLOWED)
+		{
+			(*initials_enter_complete) ();
+			initials_stop ();
+		}
+	}
 }
 
