457cd4751eda47ff621557dc39b20aadb28b03be
Brian Dominy <brian@oddchange.com>
2009-04-08 20:28:49 -0400

    Fix bug in DMD shadow function where a mysterious black bar appeared on row #2 every other time it is called.

--- platform/wpc/shadow.s
+++ platform/wpc/shadow.s
@@ -1,4 +1,4 @@
-;;; Copyright 2008 by Brian Dominy <brian@oddchange.com>
+;;; Copyright 2008, 2009 by Brian Dominy <brian@oddchange.com>
 ;;;
 ;;; This file is part of FreeWPC.
 ;;;
@@ -29,21 +29,28 @@ DMD_HIGH_BUFFER = 0x3A00
 _dmd_shadow:
 	pshs	u
 
-	; Shadow the top row of the display, by a direct copy.
-	ldx	#DMD_LOW_BUFFER
+	; Clear the first, second, and last rows of the destination
+	; buffer.  The top and bottom rows do not contain any source
+	; data, so the text should not ride up against the edge of
+	; the display.  The rows must be cleared since the page isn't
+	; guaranteed to be clean.
 	ldu	#DMD_HIGH_BUFFER
 	lda	#8
 	sta	words_per_row
-shadow_copy_top_row:
-	ldd	,x++
+	ldd	#0
+shadow_copy_clear:
+	std	0x10,u
+	std	0x1F0,u
 	std	,u++
 	dec	words_per_row
-	bne	shadow_copy_top_row
+	bne	shadow_copy_clear
+
+	ldx	#DMD_LOW_BUFFER + 16
 
 	; Shadow the middle 30 rows of the display.  For each of
 	; these rows, we output the same 16-bit into place, just
-	; above it, and just below it.  Since we go from bottom
-	; to top, the "just below" case can be set directly, but
+	; above it, and just below it.  Since we go from top to
+	; bottom, the "just below" case can be set directly, but
 	; the other two need to take into account previous iterations,
 	; and must use OR instructions.
 	; It is relatively fast because we only use 16-bit loads/stores.
@@ -63,51 +70,46 @@ shadow_copy_rows:
 	lda	#8
 	sta	words_per_row
 shadow_copy_row:
-	ldd	,x
-	std	16,u
-	ora	-16,u
-	orb	-15,u
-	std	-16,u
-	ldd	,x
-	lsla
-	rolb
-	ora	,x
-	orb	1,x
-	std	temp
-	ldd	,x++
-	lsrb
-	rora
-	ora	temp
-	orb	temp+1
-	ora	,u
-	orb	1,u
-	std	,u++
+	ldd	,x               ; Read 16 bits from source
+	std	16,u             ; Copy to the row below
+	ora	-16,u            ; OR them into the row above
+	orb	-15,u            ; ""
+	std	-16,u            ; ""
+	ldd	,x               ; Reload source data
+	lsla                   ; Shift all bits to the right
+	rolb                   ; ""
+	ora	,x               ; OR with the original source
+	orb	1,x              ; ""
+	std	temp             ; Save this
+	ldd	,x++             ; Reload source data one more time
+	lsrb                   ; Now shift all bits to the left
+	rora                   ; ""
+	ora	temp             ; Combine with original+right shifted
+	orb	temp+1           ; ""
+	ora	,u               ; OR this into the destination
+	orb	1,u              ; ""
+	std	,u++             ; ""
 	dec	words_per_row
 	bne	shadow_copy_row
 	dec	row_count
 	bne	shadow_copy_rows
 
-	; Shadow copy the bottom row.
-	lda	#8
-	sta	words_per_row
-shadow_copy_bottom_row:
-	ldd	,x++
-	std	,u++
-	dec	words_per_row
-	bne	shadow_copy_bottom_row
-
 	inc	_task_dispatching_ok
 
 	; A second pass is needed to fix up the boundary cases.
 	; There are only 15 per row, and we don't wraparound.
-	ldu	#DMD_HIGH_BUFFER + 1
-	lda	#32
+	; TODO - this is not quite right.  If we match on a pixel
+	; that was added above by shadowing, then we get a double
+	; sized shadow.  Disabling this code for now.  It really
+	; should be integrated into the above loop.
+	ldx	#DMD_LOW_BUFFER + 17
+	lda	#30
 	sta	row_count
 shadow_copy_cols2_rows:
 	lda	#15
 	sta	words_per_row
 shadow_copy_cols2_row:
-	ldd	,u
+	ldd	,x++         ; Load 16-bits from the source
 	; All that need happen is that if either the LSB of A or the
 	; MSB of B is a 1, then both need to be a 1.
 	bita	#0x80
@@ -116,23 +118,21 @@ shadow_copy_cols2_row:
 	beq	no_modify
 
 modify:
+	ldd	0x1FE,x
 	ora	#0x80
 	orb	#0x01
-	std	,u++
+	std	0x1FE,x
 	dec	words_per_row
 	bne	shadow_copy_cols2_row
 	bra	next_row
 
 no_modify:
-	leau	2,u
 	dec	words_per_row
 	bne	shadow_copy_cols2_row
 
 next_row:
-	leau	2,u
+	leax	2,x
 	dec	row_count
 	bne	shadow_copy_cols2_rows
-#if 0
-#endif
 
 	puls	u,pc
