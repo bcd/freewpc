f306d74c225c4614af8d968b0347b9c0899b9703
Brian Dominy <brian@oddchange.com>
2009-08-27 07:48:28 -0400

    TZ: Clean up display effect.

--- machine/tz/hitch.c
+++ machine/tz/hitch.c
@@ -28,7 +28,7 @@ void hitchhiker_deff (void)
 	dmd_alloc_low ();
 	frame_draw (IMG_HITCHER);
 	psprintf ("%d HITCHHIKER", "%d HITCHHIKERS", hitch_count);
-	font_render_string_center (&font_fixed6, 76, 10, sprintf_buffer);
+	font_render_string_center (&font_fixed6, 78, 10, sprintf_buffer);
 	dmd_sched_transition (&trans_scroll_left);
 	dmd_show_low ();
 	task_sleep_sec (2);
