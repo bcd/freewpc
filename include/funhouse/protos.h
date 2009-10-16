528f57f65cc7138c811f3d284327ef85755533b9
Brian Dominy <brian@oddchange.com>
2009-10-15 21:29:51 -0400

    Funhouse: Fix prototypes; fixes the lock enter bug.

--- include/funhouse/protos.h
+++ include/funhouse/protos.h
@@ -7,10 +7,10 @@ __machine__ void rudy_look_left (void);
 __machine__ void rudy_look_straight (void);
 __machine__ void rudy_look_right (void);
 __machine__ void rudy_blink (void);
-__common__ void collect_extra_ball (void);
 
 bool lock_lit_p (void);
 bool frenzy_lit_p (void);
+bool frenzy_running_p (void);
 __machine__ bool multiball_mode_running_p (void);
 
 
