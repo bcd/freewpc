0788f7c0a6ccc2beb23f88b823d24a4bd93f5412
Brian Dominy <brian@oddchange.com>
2009-10-26 08:56:31 -0400

    Fit replay adjustments on the alphanumeric display.

--- include/text.h
+++ include/text.h
@@ -9,6 +9,7 @@
 #define STR_CENTER "CTR. "
 #define STR_RIGHT "R. "
 #define STR_CREDIT "CRED. "
+#define STR_LEVEL ""
 #else
 #define STR_RECENT "RECENT "
 #define STR_PERCENT "PERCENT "
@@ -16,6 +17,7 @@
 #define STR_CENTER "CENTER "
 #define STR_RIGHT "RIGHT "
 #define STR_CREDIT "CREDIT "
+#define STR_LEVEL "LEVEL"
 #endif
 
 #endif /* _TEXT_H */
