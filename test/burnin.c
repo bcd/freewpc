797e39d5d075d9ba05edc1b6e483659d48843a34
Brian Dominy <brian@oddchange.com>
2009-11-04 21:31:37 -0500

    Track total burnin time since factory reset.

--- test/burnin.c
+++ test/burnin.c
@@ -1,9 +1,29 @@
+/*
+ * Copyright 2009 by Brian Dominy <brian@oddchange.com>
+ *
+ * This file is part of FreeWPC.
+ *
+ * FreeWPC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * FreeWPC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FreeWPC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/* CALLSET_SECTION (burnin, __test2__) */
 
 #include <freewpc.h>
 
-U16 burnin_cycles;
-
 timestamp_t burnin_duration;
+
 __nvram__ timestamp_t burnin_total_duration;
 
 void burnin_sound_thread (void)
@@ -113,13 +133,11 @@ void burnin_thread (void)
 #endif
 		task_sleep_sec (60);
 		task_kill_peers ();
-		burnin_cycles++;
 	}
 }
 
 void burnin_init (void)
 {
-	burnin_cycles = 0;
 	timestamp_clear (&burnin_duration);
 	task_create_gid (GID_WINDOW_THREAD, burnin_thread);
 	flipper_enable ();
@@ -127,9 +145,15 @@ void burnin_init (void)
 
 void burnin_exit (void)
 {
+	timestamp_add (&burnin_total_duration, &burnin_duration);
 	lamp_all_off ();
 	sound_reset ();
 	triac_leff_free (TRIAC_GI_MASK);
 	flipper_disable ();
+}
+
+CALLSET_ENTRY (burnin, factory_reset)
+{
+	timestamp_clear (&burnin_total_duration);
 }
 
