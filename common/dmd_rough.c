ee7d9708419770c6aa47ba88c63bd20601d84137
Brian Dominy <brian@oddchange.com>
2010-01-20 16:40:54 -0500

    Cleanup DMD rough functions, normalizing names and adding comments.

--- common/dmd_rough.c
+++ common/dmd_rough.c
@@ -28,6 +28,12 @@
 
 #include <freewpc.h>
 
+/*
+ * The parameters to a rough copy or erase operation.
+ * The user parameters are given in terms of pixels, however these
+ * must be converted into byte coordinates.
+ */
+
 struct dmd_rough_args
 {
 	U8 *dst;
@@ -35,13 +41,19 @@ struct dmd_rough_args
 	U8 height;
 };
 
+/* The parameter block is global to workaround the limitation of
+ * passing large numbers of arguments to the 6809 C compiler. */
 struct dmd_rough_args dmd_rough_args;
 
-/*
- * The user parameters are given in terms of pixels, however these
- * must be converted into byte coordinates.
- */
 
+/**
+ * Copy a portion of the low DMD page into the high page.
+ *
+ * x,y denote the coordinates of the upper leftmost pixel.
+ * w is the width of the area to be copied in pixels.
+ * h is the height of the area to be copied in pixels.
+ * All pixel values should be a multiple of 8 for correct results.
+ */
 #define dmd_rough_copy(x, y, w, h) \
 	do { \
 		dmd_rough_args.dst = DMD_HIGH_BASE + (x / CHAR_BIT) + y * DMD_BYTE_WIDTH; \
@@ -50,6 +62,14 @@ struct dmd_rough_args dmd_rough_args;
 		dmd_rough_copy1 (); \
 	} while (0)
 
+/**
+ * Zero a portion of the low DMD page.
+ *
+ * x,y denote the coordinates of the upper leftmost pixel.
+ * width is the width of the area to be erased in pixels.
+ * height is the height of the area to be erased in pixels.
+ * All pixel values should be a multiple of 8 for correct results.
+ */
 #define dmd_rough_erase(x, y, width, height) \
 	do { \
 		dmd_rough_args.dst = DMD_HIGH_BASE + (x / CHAR_BIT) + y * DMD_BYTE_WIDTH; \
@@ -58,11 +78,13 @@ struct dmd_rough_args dmd_rough_args;
 		dmd_rough_erase1 (); \
 	} while (0)
 
-#define args dmd_rough_args
 
 
 /**
  * Copy a portion of the low DMD page into the high page.
+ *
+ * Use the function dmd_rough_copy() instead of this one.  This function
+ * expects the global parameter block 'dmd_rough_args' to be filled in.
  */
 __attribute__((noinline)) void dmd_rough_copy1 (void)
 {
@@ -70,41 +92,45 @@ __attribute__((noinline)) void dmd_rough_copy1 (void)
 	U8 *src;
 
 	do {
-		dst = args.dst;
+		dst = dmd_rough_args.dst;
 		src = dst - DMD_PAGE_SIZE;
-		U8 bytes = args.bwidth;
+		U8 bytes = dmd_rough_args.bwidth;
 		do {
 			*dst++ = *src++;
 			bytes--;
 		} while (bytes > 0);
-		args.dst += DMD_BYTE_WIDTH;
-		args.height--;
-	} while (args.height > 0);
+		dmd_rough_args.dst += DMD_BYTE_WIDTH;
+		dmd_rough_args.height--;
+	} while (dmd_rough_args.height > 0);
 }
 
 
 /**
- * Zero a portion of the low DMD page into the high page.
+ * Zero a portion of the low DMD page.
+ *
+ * Use the function dmd_rough_erase() instead of this one.  This function
+ * expects the global parameter block 'dmd_rough_args' to be filled in.
  */
 __attribute__((noinline)) void dmd_rough_erase1 (void)
 {
 	U8 *dst;
 
 	do {
-		dst = args.dst;
-		U8 bytes = args.bwidth;
+		dst = dmd_rough_args.dst;
+		U8 bytes = dmd_rough_args.bwidth;
 		do {
 			*dst++ = 0;
 			bytes--;
 		} while (bytes > 0);
-		args.dst += DMD_BYTE_WIDTH;
-		args.height--;
-	} while (args.height > 0);
+		dmd_rough_args.dst += DMD_BYTE_WIDTH;
+		dmd_rough_args.height--;
+	} while (dmd_rough_args.height > 0);
 }
 
 
+#if 1 /* for testing */
 void dmd_rough_test (void)
 {
 	dmd_rough_copy (32, 4, 64, 4);
 }
-
+#endif
