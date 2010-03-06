b8b3719037072178ac7fbdbee7ede2f5fa5fd293
Sonny_Jim <sonny_jim@hotmail.com>
2010-03-06 18:27:21 +0000

    Updated rules

--- machine/tz/rules.c
+++ machine/tz/rules.c
@@ -22,10 +22,20 @@
 
 #include <freewpc.h>
 
+typedef enum {
+	RULES_SPIRALAWARD=0,
+	RULES_INLANES,
+	RULES_CHAOSMB,
+	RULES_SSSMB,
+	RULES_GUMBALL,
+	RULES_EXIT,
+} rules_page_t;
+
+rules_page_t rule_page;
+
 void rule_begin (void)
 {
 	lamp_all_off ();
-	triac_disable (TRIAC_GI_MASK);
 }
 
 static void rule_msg (const char *line1, const char *line2, const char *line3, const char *line4)
@@ -36,7 +46,7 @@ static void rule_msg (const char *line1, const char *line2, const char *line3, c
 	font_render_string_center (&font_var5, 64, 20, line3)
 	font_render_string_center (&font_var5, 64, 27, line4)
 	dmd_sched_transition (&trans_sequential_boxfade);
-	dmd_show2 ();
+	dmd_show_low ();
 }
 
 void rule_complete (void)
@@ -45,53 +55,136 @@ void rule_complete (void)
 	task_kill_gid (GID_RULES_LEFF);
 }
 
-void rules_spiralaward_inlanes_leff (void)
-{
-	lamp_tristate_flash (LM_LEFT_INLANE1);
-	lamp_tristate_flash (LM_LEFT_INLANE2);
-	task_sleep_sec (3);
-	lamp_tristate_off (LM_LEFT_INLANE1);
-	lamp_tristate_off (LM_LEFT_INLANE2);
-	lamplist_apply (LAMPLIST_SPIRAL_AWARDS, lamp_flash);
-}
-
-void rules_spiralaward_loop_leff (void)
-{
-	lamp_tristate_flash (LM_RIGHT_SPIRAL);
-}
-
 void rules_leff (void)
 {
+}
+
+void rules_rollover_leff (void)
+{
+	triac_disable (TRIAC_GI_MASK);
+	for (;;)
+	{
+		lamp_tristate_off (LM_RIGHT_INLANE);
+		lamp_tristate_on (LM_LEFT_INLANE1);
+		task_sleep_sec (1);
+		lamp_tristate_off (LM_LEFT_INLANE1);
+		lamp_tristate_on (LM_LEFT_INLANE2);
+		task_sleep_sec (1);
+		lamp_tristate_off (LM_LEFT_INLANE2);
+		lamp_tristate_on (LM_RIGHT_INLANE);
+		task_sleep_sec (1);
+	}
+}
+
+void rules_sssmb_leff (void)
+{
+	triac_disable (TRIAC_GI_MASK);
+	for (;;)
+	{
+		lamp_tristate_off (LM_BONUS_X);
+		lamp_tristate_flash (LM_SUPER_SKILL);
+		task_sleep (TIME_500MS);
+		lamp_tristate_off (LM_SUPER_SKILL);
+		lamp_tristate_flash (LM_MULTIBALL);
+		task_sleep (TIME_500MS);
+		lamp_tristate_off (LM_MULTIBALL);
+		lamp_tristate_flash (LM_BONUS_X);
+		task_sleep (TIME_500MS);
+	}
+}
+
+void rules_chaosmb_leff (void)
+{
+	triac_disable (TRIAC_GI_MASK);
+	lamp_tristate_flash (LM_CLOCK_MILLIONS);
+	task_sleep_sec (3);
+	for (;;)
+	{
+		lamp_tristate_flash (LM_SUPER_SKILL);
+		task_sleep (TIME_500MS);
+		lamp_tristate_off (LM_SUPER_SKILL);
+		lamp_tristate_flash (LM_MULTIBALL);
+		task_sleep (TIME_500MS);
+		lamp_tristate_off (LM_MULTIBALL);
+		lamp_tristate_flash (LM_BONUS_X);
+		task_sleep (TIME_500MS);
+		lamp_tristate_off (LM_BONUS_X);
+
+		lamp_tristate_flash (LM_RAMP_BATTLE);
+		task_sleep (TIME_500MS);
+		lamp_tristate_off (LM_RAMP_BATTLE);
+		lamp_tristate_flash (LM_PIANO_JACKPOT);
+		task_sleep (TIME_500MS);
+		lamp_tristate_off (LM_PIANO_JACKPOT);
+		lamp_tristate_flash (LM_CAMERA);
+		task_sleep (TIME_500MS);
+		lamp_tristate_off (LM_CAMERA);
+		lamp_tristate_flash (LM_POWER_PAYOFF);
+		task_sleep (TIME_500MS);
+		lamp_tristate_off (LM_POWER_PAYOFF);
+	}
+}
+
+void rules_spiralaward_leff (void)
+{
+	for (;;)
+	{
+		lamp_tristate_off (LM_LEFT_INLANE2);
+		lamp_tristate_flash (LM_LEFT_INLANE1);
+		task_sleep (TIME_500MS);
+		lamp_tristate_off (LM_LEFT_INLANE1);
+		lamp_tristate_flash (LM_LEFT_INLANE2);
+		task_sleep (TIME_500MS);
+	}
 }
 
 void rules_deff (void)
 {
 	music_disable ();
+	leff_stop_all ();
 
 	rule_begin ();
 	rule_msg ("BACK TO THE ZONE", "", "HOW TO PLAY", "");
 	rule_complete ();
 
+	rule_page = RULES_SPIRALAWARD;
 	rule_begin ();
 	rule_msg ("SPIRALAWARD", "EITHER LEFT INLANE STARTS", "A 3 SECOND TIMER", "");
-	task_create_gid1 (GID_RULES_LEFF, rules_spiralaward_inlanes_leff);
+	task_create_gid1 (GID_RULES_LEFF, rules_spiralaward_leff);
 	rule_complete ();
 	
 	rule_begin ();
-	rule_msg ("SPIRALAWARD", "SHOOT A RIGHT LOOP", "TO COLLECT A RANDOM AWARD", "20 MILLION BONUS FOR COLLECTING ALL");
-	task_create_gid1 (GID_RULES_LEFF, rules_spiralaward_loop_leff);
+	rule_msg ("SPIRALAWARD", "SHOOT A RIGHT LOOP TO COLLECT", "20 MILLION BONUS" , "FOR COLLECTING ALL");
+	lamplist_apply (LAMPLIST_SPIRAL_AWARDS, lamp_flash_on);
+	lamp_tristate_flash (LM_RIGHT_SPIRAL);
+	lamp_tristate_flash (LM_GUMBALL_LANE);
+	task_sleep_sec (2);
+	lamplist_apply (LAMPLIST_SPIRAL_AWARDS, lamp_flash_off);
+	rule_complete ();
+
+	rule_begin ();
+	rule_msg ("ROLLOVERS", "USE FLIPPER BUTTONS TO STEP", "INLANE LIGHTS", "1M FOR COLLECTING ALL");
+	task_create_gid1 (GID_RULES_LEFF, rules_rollover_leff);
+	rule_complete ();
+	
+	rule_begin ();
+	rule_msg ("SUPER SKILL MB", "SHOOT LEFT RAMP", "AND HIT SKILL SHOT", "DURING MULTIBALL");
+	task_create_gid1 (GID_RULES_LEFF, rules_sssmb_leff);
+	rule_complete ();
+
+	rule_begin ();
+	rule_msg ("CHAOS MULTIBALL", "HIT CLOCK TO LIGHT JACKPOTS", "JACKPOTS MOVE", "AROUND THE TABLE");
+	task_create_gid1 (GID_RULES_LEFF, rules_chaosmb_leff);
+	task_sleep_sec (3);
 	rule_complete ();
 
 	music_enable ();
+	leff_start (LEFF_AMODE);
 	deff_exit ();
 }
 
 CALLSET_ENTRY (rules, sw_buyin_button)
 {
 	if (!in_game && !in_test)
-	{
-		//leff_start (LEFF_RULES);
-		leff_stop_all ();
 		deff_start (DEFF_RULES);
-	}
 }
