17d990d9d8682e51d37f0ec0c6103126369cd762
Brian Dominy <brian@oddchange.com>
2010-08-23 22:18:07 -0400

    Move scanbit() out of native mode main.c

--- platform/native/main.c
+++ platform/native/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006, 2007, 2008, 2009, 2010 by Brian Dominy <brian@oddchange.com>
+ * Copyright 2006-2010 by Brian Dominy <brian@oddchange.com>
  *
  * This file is part of FreeWPC.
  *
@@ -189,22 +189,6 @@ void simlog (enum sim_log_class class, const char *format, ...)
 	}
 
 	va_end (ap);
-}
-
-
-/** Find the highest numbered bit set in a bitmask.
- * Returns -1 if no bits are set. */
-static int scanbit (U8 bits)
-{
-	if (bits & 0x80) return 7;
-	else if (bits & 0x40) return 6;
-	else if (bits & 0x20) return 5;
-	else if (bits & 0x10) return 4;
-	else if (bits & 0x08) return 3;
-	else if (bits & 0x04) return 2;
-	else if (bits & 0x02) return 1;
-	else if (bits & 0x01) return 0;
-	else return -1;
 }
 
 
