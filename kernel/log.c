0d25b5e9ce097cffe04c5ca7a39effa1e1d2dec1
Brian Dominy <brian@oddchange.com>
2010-09-30 17:00:03 -0400

    Disable the logging facility.  It is not being used.
    The code is retained under CONFIG_LOG for now.

--- kernel/log.c
+++ kernel/log.c
@@ -92,6 +92,7 @@ char *log_get_format (U16 module_event)
 
 
 /** Add an entry to the event log. */
+#ifdef CONFIG_LOG
 void log_event1 (U16 module_event, U8 arg)
 {
 	struct log_event *ev;
@@ -129,6 +130,7 @@ void log_event1 (U16 module_event, U8 arg)
 		log_get_format (module_event), arg);
 #endif
 }
+#endif /* CONFIG_LOG */
 
 
 /** Initialize the event log. */
