6a32622adedd4a9b4f5e3998f05c7480b8de3dfd
Sonny_Jim <sonny_jim@hotmail.com>
2011-04-11 21:07:42 +0000

    Tidy up alarm.c, still needs work

--- machine/tz/alarm.c
+++ machine/tz/alarm.c
@@ -22,12 +22,57 @@
 
 /* Turns a pinball table into an Egg Timer */
 #include <freewpc.h>
-/* TODO Store a specific time and watch the rtc instead */
-__permanent__ bool alarm_enabled;
-/* Time in minutes before the alarm goes off */
-__permanent__ U8 alarm_timer;
+/* 1440 minutes in a day */
+#define ALARM_DISABLED 1441
+
+/* alarm_time is stored as minutes from midnight */
+__permanent__ U16 alarm_time;
+__permanent__ bool alarm_persistant;
 
 extern U8 mute_and_pause_timeout;
+
+/* How long in minutes before the alarm goes off */
+static void render_alarm_time (void)
+{
+	static const U8 rtc_us_hours[] = {
+		12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+		12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+	};
+
+	switch (system_config.clock_style)
+	{
+		case CLOCK_STYLE_AMPM:
+		default:
+			sprintf ("%d:%02d %s",
+				rtc_us_hours[alarm_hour], alarm_minute, (alarm_hour >= 12)?"PM":"AM");
+			break;
+
+		case CLOCK_STYLE_24HOUR:
+			sprintf ("%02d:%02d", alarm_hour, alarm_minute);
+			break;
+	}
+}
+
+static void render_minutes_to_alarm (void)
+{
+	U16 minutes_to_alarm;
+
+	char s;
+	if (alarm_hour > hour)
+	{	
+		minutes_to_alarm = ((alarm_hour - hour) * 60);
+		minutes_to_alarm += alarm_minute;
+	}
+	else if (alarm_hour < hour)
+	{
+		minutes_to_alarm = (24 - hour) - alarm_hour;minutes_to_alarm
+		minutes_to_alarm += alarm_minute;
+		//Erm....
+	}
+	else if (alarm_hour = hour)
+		minutes_to_alarm = alarm_minute - minute;
+	sprintf ("ALARM IN %s MINUTES", minutes_to_alarm);
+}
 
 void paused_deff (void)
 {
@@ -36,18 +81,19 @@ void paused_deff (void)
 		dmd_alloc_pair_clean ();
 		
 		font_render_string_center (&font_fixed10, 64, 10, "PAUSED");
-		/* mute_and_pause_timeout is stored as 5 second chunks, to save
-		 * having to use a U16 */
-		if (!alarm_enabled)
+		if (alarm_hour >= ALARM_DISABLED)
 		{
+			/* mute_and_pause_timeout is stored as 5 second chunks, to save
+			 * having to use a U16 */
 			if (mute_and_pause_timeout <= 12)
 				sprintf ("TIMEOUT IN %d SECONDS", mute_and_pause_timeout * 5);
 			else
 				sprintf ("TIMEOUT IN %d MINUTES", mute_and_pause_timeout / 12);
 		}
 		else
-			sprintf ("ALARM IN %d MINUTES", alarm_timer);
-			
+		{
+			render_minutes_to_alarm ();
+		}
 		font_render_string_center (&font_var5, 64, 20, sprintf_buffer);
 		font_render_string_center (&font_var5, 64, 27, "PRESS BUYIN TO CONTINUE");
 		dmd_show_low ();
@@ -70,61 +116,9 @@ void alarm_deff (void)
 	deff_exit ();
 }
 
-CALLSET_ENTRY (alarm, amode_page)
-{
-	if (alarm_enabled)
-	{
-		dmd_alloc_pair_clean ();
-		sprintf ("ALARM IN %d MINUTES", alarm_timer);
-		font_render_string_center (&font_var5, 64, 16, sprintf_buffer);
-		show_text_on_stars ();
-	}
-}
-
-CALLSET_ENTRY (alarm, sw_left_button)
-{
-	if (deff_get_active () == DEFF_PAUSED)
-	{
-		bounded_decrement (alarm_timer, 0);
-		if (alarm_timer == 0)
-			alarm_enabled = FALSE;
-		else
-		{
-			while (switch_poll_logical (SW_LEFT_BUTTON))
-			{
-				task_sleep (TIME_200MS);
-				bounded_decrement (alarm_timer, 0);
-			}
-		}
-	}
-}
-
-CALLSET_ENTRY (alarm, sw_right_button)
-{
-	if (deff_get_active () == DEFF_PAUSED)
-	{
-		alarm_enabled = TRUE;
-		bounded_increment (alarm_timer, 60);
-		if (alarm_timer < 60)
-		{
-			while (switch_poll_logical (SW_RIGHT_BUTTON))
-			{
-				task_sleep (TIME_200MS);
-				bounded_increment (alarm_timer, 60);
-			}
-		}
-	}
-}
-
-CALLSET_ENTRY (alarm, factory_reset)
-{
-	alarm_enabled = FALSE;
-	alarm_timer = 4;
-}
-
 static void alarm_task (void)
 {
-	alarm_enabled = FALSE;
+	alarm_hour = ALARM_DISABLED;
 	U8 i;
 	for (i = 0; i < 4; i++)
 	{
@@ -135,15 +129,77 @@ static void alarm_task (void)
 	task_exit ();
 }
 
+static inline bool check_alarm_time (void)
+{
+	if (hour >= ALARM_DISABLED)
+		return FALSE;
+	/* This should cover cases where the machine was switched off before the
+	 * alarm could sound */
+	else if (hour >= alarm_hour && minute >= alarm_minute)
+		return TRUE;
+	else
+		return FALSE;
+}
+
 CALLSET_ENTRY (alarm, minute_elapsed)
 {
-	if (alarm_enabled)
-	{
-		bounded_decrement (alarm_timer, 0);
-		if (alarm_timer == 0)
-			task_create_anon (alarm_task);
-	}	
+	if (check_alarm_time ())
+		task_create_anon (alarm_task);
 }
+
+CALLSET_ENTRY (alarm, amode_page)
+{
+	if (alarm_hour < ALARM_DISABLED)
+	{
+		dmd_alloc_pair_clean ();
+		sprintf ("ALARM IN %d MINUTES", alarm_timer);
+		font_render_string_center (&font_var5, 64, 16, sprintf_buffer);
+		show_text_on_stars ();
+	}
+}
+
+CALLSET_ENTRY (alarm, sw_start_button)
+{
+	if (deff_get_active () == DEFF_PAUSED)
+	{
+		alarm_time = ALARM_DISABLED;
+	}
+}
+
+CALLSET_ENTRY (alarm, sw_left_button)
+{
+	if (deff_get_active () == DEFF_PAUSED)
+	{
+		timer_restart_gid (GID_ALARM_BUTTON_PUSH, TIME_2S);
+		bounded_decrement (alarm_time, 0);
+		while (switch_poll_logical (SW_RIGHT_BUTTON))
+			{
+				task_sleep (TIME_200MS);
+				bounded_decrement (alarm_time, 0);
+			}
+
+	}
+}
+
+CALLSET_ENTRY (alarm, sw_right_button)
+{
+	if (deff_get_active () == DEFF_PAUSED)
+	{
+		timer_restart_gid (GID_ALARM_BUTTON_PUSH, TIME_2S);
+		bounded_increment (alarm_time, 0);
+			while (switch_poll_logical (SW_RIGHT_BUTTON))
+			{
+				task_sleep (TIME_200MS);
+				bounded_increment (alarm_time, ALARM_DISABLED - 1);
+			}
+	}
+}
+
+CALLSET_ENTRY (alarm, factory_reset)
+{
+	alarm_time = ALARM_DISABLED;
+}
+
 
 CALLSET_ENTRY (alarm, display_update)
 {
