250390960b3e8c03f0c2c228776fb67006dc9072
Brian Dominy <brian@oddchange.com>
2012-02-28 22:41:22 -0500

    Update minimal platform example for Beaglebone:
    - Enable generic POSIX input module
    - Remove CPU specific init that is done elsewhere
    - LED refresh will not rewrite a GPIO with the same value.
    - Chose a few GPIOs for solenoids.
    - Added a simple amode lamp effect to test the LEDs.

--- platform/min/min-hw.c
+++ platform/min/min-hw.c
@@ -2,11 +2,19 @@
 #include <freewpc.h>
 #include "system/platform.h"
 #include "native/gpio.h"
+#include "native/log.h"
 
 /* RTTX(name=triac_rtt   freq=1) */
 /* RTTX(name=pinio_active_led_toggle   freq=64) */
 /* RTTX(name=sound_write_rtt   freq=2) */
 
+#define GPIO_SOL_0 GPID(1,6)
+#define GPIO_SOL_1 GPID(1,7)
+#define GPIO_SOL_2 GPID(1,2)
+#define GPIO_SOL_3 GPID(1,3)
+
+U8 last_lamps;
+U8 last_sols;
 
 #ifndef CONFIG_SIM
 void writeb (IOPTR ptr, U8 val)
@@ -14,10 +22,24 @@ void writeb (IOPTR ptr, U8 val)
 	switch (ptr)
 	{
 		case IO_LAMP:
-			gpio_write_led (0, val & 1);
-			gpio_write_led (1, val & 2);
-			gpio_write_led (2, val & 4);
-			gpio_write_led (3, val & 8);
+			if (val != last_lamps)
+			{
+				gpio_write_led (0, val & 1);
+				gpio_write_led (1, val & 2);
+				gpio_write_led (2, val & 4);
+				gpio_write_led (3, val & 8);
+				last_lamps = val;
+			}
+			break;
+		case IO_SOL:
+			if (val != last_sols)
+			{
+				gpio_write (GPIO_SOL_0, val & 1);
+				gpio_write (GPIO_SOL_1, val & 2);
+				gpio_write (GPIO_SOL_2, val & 4);
+				gpio_write (GPIO_SOL_3, val & 8);
+				last_sols = val;
+			}
 			break;
 	}
 }
@@ -35,6 +57,12 @@ void switch_rtt (void)
 {
 	platform_switch_input (0, readb (IO_SWITCH));
 	platform_switch_debounce (0);
+	int c = posix_input_poll (0);
+	if (c > 0)
+	{
+		if (c == ' ')
+			print_log ("space found\n");
+	}
 }
 
 /* RTT(name=lamp_rtt freq=16) */
@@ -56,5 +84,27 @@ void sol_update_rtt_1 (void)
 void platform_init (void)
 {
 	/* Request GPIOs */
+	gpio_request_output (GPIO_SOL_0);
+	gpio_request_output (GPIO_SOL_1);
+	gpio_request_output (GPIO_SOL_2);
+	gpio_request_output (GPIO_SOL_3);
+
+	posix_input_config (0);
+
+	last_lamps = last_sols = 0;
+}
+
+void amode_leff (void)
+{
+	for (;;)
+	{
+		leff_on (LM_SHOOT_AGAIN);
+		task_sleep (TIME_166MS);
+		leff_off (LM_SHOOT_AGAIN);
+		leff_on (LM_TARGET);
+		task_sleep (TIME_166MS);
+		leff_off (LM_TARGET);
+		task_sleep (TIME_166MS);
+	}
 }
 
--- platform/min/min.md
+++ platform/min/min.md
@@ -6,15 +6,22 @@ include platform/generic.md
 #drives-numbered: integer
 #gi-numbered: integer
 
+define MACHINE_CUSTOM_AMODE
+
+# The system needs a minimum of 4 switches to compile: a
+# start button, two flipper buttons, and a trough switch.
+# A 5th scoring target is needed in order to cycle through a
+# game - otherwise valid playfield can never be set.
+
 [switches]
 D1: Start Button, start-button, debounce(TIME_100MS)
-D2: Left Flipper, cabinet
-D3: Right Flipper, cabinet
+D2: Left Button, cabinet
+D3: Right Button, cabinet
 D4: Trough, noscore
 D5: Target, ingame
 
 [lamps]
-11: Start Button, start-button
+11: Start Button, start
 12: Shoot Again, shoot-again, ball-save
 13: Target
 14: GI
@@ -24,8 +31,18 @@ H1: Ball Release
 H2: Left Flipper
 H3: Right Flipper
 
+# The ball save requires a lamplist in order to denote ball
+# save active as a lamp effect.
+
 [lamplists]
 Ball Save: Shoot Again
+Playfield: Shoot Again, Target
+
+# There must be a trough device
 
 [containers]
 Trough: Ball Release, trough, init_max_count(1), Trough
+
+[leffs]
+Amode: runner, PRI_AMODE, c_decl(amode_leff)
+
