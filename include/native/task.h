c1314fe11f28d2fb43604e142eb15dd178f7d77f
Brian Dominy <brian@oddchange.com>
2012-10-03 10:52:05 -0400

    Move native-mode task definitions to a separate file.
    task.h was getting too complicated to understand, so copy it
    and strip out all the non-native stuff.  This will make changes
    to it easier.
    
    Likewise, remove all the native stuff from the original file.

--- include/native/task.h
+++ include/native/task.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2012 by Brian Dominy <brian@oddchange.com>
+ * Copyright 2012 by Brian Dominy <brian@oddchange.com>
  *
  * This file is part of FreeWPC.
  *
@@ -33,158 +33,29 @@ extern bool task_dispatching_ok;
 #define TASK_DURATION_GAME 0x7
 #define TASK_DURATION_TEST 0x8
 
-#include <env.h>
+#include <sys/time.h>
+#define USECS_PER_TICK (16000 / linux_irq_multiplier)
+#if defined(CONFIG_PTH)
+#include <pth.h>
+typedef pth_t task_pid_t;
+#elif defined(CONFIG_PTHREADS)
+#undef __noreturn__
+#include <pthread.h>
+#define __noreturn__ __attribute__((noreturn))
+typedef pthread_t task_pid_t;
+#else
+typedef int task_pid_t;
+#endif
 
-/** Values for the 'state' field in the task structure */
-#define BLOCK_FREE 0x0
-
-/* Says that the block is in use */
-#define BLOCK_USED  0x1
-
-/* Says that the block is used by the 'malloc' function */
-#define BLOCK_MALLOC 0x2
-
-/* Says that the block is used by the task scheduler */
-#define BLOCK_TASK 0x4
-
-/* Says that the block is used to hold a task stack */
-#define BLOCK_STACK	0x8
-
-/* Says that the task is in the blocked state */
-#define TASK_BLOCKED 0x10
-
-
-/** Define the size of the saved process stack. */
-#define TASK_STACK_SIZE 40
-
-
-
-/** Type for the group ID (gid) */
-typedef U8 task_gid_t;
-
-/** Type for the number of task ticks.  1 task tick is the
- * smallest amount that a task can sleep. */
-typedef U8 task_ticks_t;
-
-/** Type for the entry point to a new task */
+typedef unsigned int task_gid_t;
+typedef unsigned int task_ticks_t;
 typedef void (*task_function_t) (void);
-
-/** Type for the task structure.
- * For proper saving/restoring, only the registers that are live
- * across function calls need to be kept here.  The 6809 compiler
- * allows functions to trash D and X, so they aren't here -- just
- * U and Y.
- *
- * When adding/removing fields to this structure, adjust
- * TASK_STACK_SIZE above accordingly so that the total size is
- * suitably aligned.
- */
-typedef struct task_struct
-{
-	/** The execution state of the task.  It can be BLOCK_FREE, if the
-	 * task entry isn't being used at all; BLOCK_USED for a running/waiting
-	 * task; or TASK_BLOCKED for a sleeping task. */
-	U8				state;
-
-	/** The index of the next block in the same chain.  Not used yet.
-	 * A NULL is indicated by a -1, hence it is signed. */
-	S8          chain;
-
-	/** The task group ID.  This is a compile-time assigned value
-	 * used to identify the task.   Multiple running tasks can also
-	 * share the same group ID; operations on a group will affect
-	 * *all* tasks with that ID, in most cases. */
-	task_gid_t	gid;
-
-	/** The saved PC for the task, while it is blocked */
-	U16			pc;
-
-	/** The saved Y register for the task, while it is blocked */
-	U16			y;
-
-	/** The saved U register for the task, while it is blocked */
-	U16			u;
-
-	/** The saved ROM page register for the task, while it is blocked */
-	U8				rom_page;
-
-	/** The amount of stack space, in bytes, that has been saved into
-	 * the task's stack area */
-	U8				stack_size;
-
-	/** The time at which the task should be awakened, when it is blocked.
-	More correctly, the system time must be greater than this value for the
-	task to run again.  It will not run when the two are equal. */
-	U16			wakeup;
-
-	/** The task argument pointer.  This is a primitive way of passing
-	 * initialization data to a new task.  The creator of the task can
-	 * assign this argument pointer, after creating the task but before
-	 * the next schedule. */
-	/* TODO - few tasks need an arg, and this is wasting RAM for those
-	 * that don't.  Better to push any arguments onto the new task's stack
-	 * and declare the task function to accept those arguments, with
-	 * an ellipsis to prevent assuming they are in registers. */
-	PTR_OR_U16	arg;
-
-	/** The index of an auxiliary memory block used to store
-	 * additional stack data.  This is used when the stack space
-	 * within the task block itself is not large enough.  When set
-	 * to -1, it means there is no auxiliary storage. */
-	U8				aux_stack_block;
-
-	/** Says how long the task is allowed to exist, before being
-	 * stopped automatically due to some external event. */
-	U8				duration;
-
-	/** Not currently used */
-	U8				reserved;
-
-	/** The task stack save area.  This is NOT used as the live stack
-	 * area; the live stack is copied here when the task blocks.
-	 * Because of this, tasks can use a much larger stack size if needed,
-	 * as long as they don't try to block while holding that much space.
-	 * Practically, this means that you shouldn't sleep in a deeply
-	 * nested set of function calls. */
-	U8				stack[TASK_STACK_SIZE];
-} task_t;
-
-
-
-/********************************/
-/*     Inline Macros            */
-/********************************/
-
-extern inline task_t *task_getpid (void)
-{
-	extern task_t *task_current;
-	return task_current;
-}
-
-extern inline task_gid_t task_getgid (void)
-{
-	return task_getpid ()->gid;
-}
-
-extern inline void task_set_rom_page (task_t *pid, U8 rom_page)
-{
-	pid->rom_page = rom_page;
-}
-
-/** A process ID, or PID, is just a pointer to the task block.
- * PIDs are rarely used as they are dynamic in value. */
-typedef task_t *task_pid_t;
+#define task_set_rom_page(pid, page)
 
 
 /********************************/
 /*     Function Prototypes      */
 /********************************/
-
-task_t *block_allocate (void);
-void malloc_chunk_dump (task_t *task);
-#ifdef CONFIG_MALLOC
-void block_free (task_t *tp);
-#endif
 
 void task_dump (void);
 void task_init (void);
@@ -214,6 +85,8 @@ void task_set_arg (task_pid_t tp, U16 arg);
 void *task_get_pointer_arg (void);
 void task_set_pointer_arg (task_pid_t tp, void *arg);
 __noreturn__ void task_dispatcher (task_pid_t tp);
+task_pid_t task_getpid (void);
+task_gid_t task_getgid (void);
 void do_periodic (void);
 
 /** Create a new task that has the same group ID as the current one. */
@@ -317,32 +190,12 @@ far_task_recreate_gid (task_gid_t gid, task_function_t fn, U8 page)
 #define task_do_periodic(time) \
 	for (task_set_periodic (time);; task_sleep_periodic (), task_set_periodic (time))
 
-#define TASK_DECL_ARGS(args...) args, ...
+extern void *task_get_class_data (task_pid_t);
+extern void task_set_class_data (task_pid_t, size_t);
 
-#define TASK_GET_ARG(arg) arg
+#define task_class_data(tp, type) ((type *)task_get_class_data (tp))
 
-#define task_top_of_stack(tp)	(&tp->stack[TASK_STACK_SIZE - tp->stack_size])
-
-#define task_push_arg(tp, arg) \
-do { \
-	tp->stack_size += sizeof (arg); \
-	*((typeof (arg) *)task_top_of_stack(tp)) = arg; \
-} while (0)
-
-#define task_push_args_done(tp) task_push_arg(tp, ((U16)task_exit))
-
-
-/** Returns the pointer to the class data for a task.
- * This is a structure located at the top of the task's
- * stack in a fixed location, where it is visible to other
- * tasks as well.
- * tp identifies the task.  type is the C structure that says
- * how the area should be viewed.
- */
-#define task_class_data(tp, type) ((type *)(&(tp)->stack[0]))
-
-/** Initializes the class data area for a new task. */
-#define task_init_class_data(tp, type) ({ task_class_data (tp, type); })
+#define task_init_class_data(tp, type) ({ task_set_class_data (tp, sizeof (type)); task_class_data (tp, type); })
 
 /** Returns the pointer to the class data for the current task. */
 #define task_current_class_data(type) task_class_data(task_getpid (), type)
